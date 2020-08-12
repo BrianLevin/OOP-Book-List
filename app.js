634538246575e7abb1234a700ab4799ac409b74e
Brian Levin <bml201095@gmail.com>
2020-08-12 14:21:30 -0400

    made an if else staement to validate thr fields

--- app.js
+++ app.js
@@ -46,12 +46,22 @@ document.getElementById('book-form').addEventListener('submit', function(e){
   // Instantiate UI
   const ui = new UI();
 
-  
-  // Add book to list
-  ui.addBookToList(book);
+   // Validate
+   if(title === '' || author === '' || isbn === '') {
+    // Error alert
+    ui.showAlert('Please fill in all fields', 'error');
+  } else {
+// Add book to list
+ui.addBookToList(book);
 
- // Clear fields
-  ui.clearFields();
+// Clear fields
+ ui.clearFields();
+
+  }
+
+  
+
+  
 
   e.preventDefault();
 });
