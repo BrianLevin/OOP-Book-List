83d7451594ec06dd894fbb2b6df6d26a85070ae7
Brian Levin <bml201095@gmail.com>
2020-08-12 11:48:19 -0400

    added event listener to get form values

--- app.js
+++ app.js
@@ -7,3 +7,15 @@ function Book(title, author, isbn) {
 
   // UI Constructor
 function UI() {}
+
+
+
+// Event Listeners
+document.getElementById('book-form').addEventListener('submit', function(e){
+    // Get form values
+    const title = document.getElementById('title').value,
+          author = document.getElementById('author').value,
+          isbn = document.getElementById('isbn').value
+
+
+});
